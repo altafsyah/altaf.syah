079da757494e75714ee2133c3b0f7a8122542bb0
altafsyah <syahrastanialtaf@gmail.com>
2023-07-22 18:39:53 +0700

    fix footer error

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const LIST_IMAGE = {
-  1: "/icon-home.svg",
-  2: "/icon-camera.svg",
-  3: "/icon-rocket.svg",
-  4: "/icon-user.svg",
-  5: "/icon-uses.svg",
-}
+const LIST_IMAGE: string[]  = [
+  "/icon-home.svg",
+  "/icon-camera.svg",
+  "/icon-rocket.svg",
+  "/icon-user.svg",
+  "/icon-uses.svg",
+]
 
 function FooterLink({ to, text, isActive ,index }: { to: string; text?: string, isActive: boolean, index: number }) {
   return (
@@ -25,19 +25,19 @@ export default function Footer({ index }: { index: number}) {
     <footer className="mx-auto px-5 py-3 rounded-xl fixed bg-white backdrop-blur-xl bottom-5  right-1/2 translate-x-1/2 lg:static lg:translate-x-0">
       <ul className="flex gap-x-5">
         <li>
-          <FooterLink to="/" isActive={index == 1} index={1}/>
+          <FooterLink to="/" isActive={index == 0} index={0}/>
         </li>
         <li>
-          <FooterLink to="/about" isActive={index == 2} index={2}/>
+          <FooterLink to="/about" isActive={index == 1} index={1}/>
         </li>
         <li>
-          <FooterLink to="/project" isActive={index == 3} index={3}/>
+          <FooterLink to="/project" isActive={index == 2} index={2}/>
         </li>
         <li>
-          <FooterLink to="/journey" isActive={index == 4} index={4}/>
+          <FooterLink to="/journey" isActive={index == 3} index={3}/>
         </li>
         <li>
-          <FooterLink to="/uses" isActive={index == 5} index={5}/>
+          <FooterLink to="/uses" isActive={index == 4} index={4}/>
         </li>
       </ul>
     </footer>
